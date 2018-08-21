f297181e6df6a29c7d4aa0b31bf466b37ff1e8fb
William Lathrop <wlathrop10@gmail.com>
2018-08-21 19:50:41 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-
+  if speed > 60 || speed < 40
+      return TRUE
+  else
+      return FALSE
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false 
 end
 	
 
